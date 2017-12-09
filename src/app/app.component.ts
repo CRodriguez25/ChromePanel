6b4e9363af853ee82c15b7d8d41879254c84e2f2
Carlos Rodriguez <cmrodriguez25@gmail.com>
2017-12-09 07:02:30 +0000

    iInitial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgxCarousel } from 'ngx-carousel';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+	public carouselOne: NgxCarousel;
+	ngOnInit() {
+		this.carouselOne = {
+			grid: {xs: 1, sm: 1, md: 1, lg: 1, all: 0},
+			slide: 1,
+			speed: 400,
+			interval: 4000,
+			point: {
+				visible: true
+			},
+			load: 2,
+			touch: true,
+			loop: true
+		}
+	}
+	
+	public myfunc(event: Event) {
+		
+	}
 }
